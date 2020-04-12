5cfe4e3584cb978603105543d24235303c599c57
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-12 12:05:24 +0530

    grade c greater than 70

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ if(grade <= 100 && grade >= 90){
     console.log('Your got a ' + 'A' + '(' + grade + '%' + ')!')
 }else if(grade >= 80){
  console.log('Your got a '+ 'B' + '(' + grade + '%' + ')!')
+}else if(grade >= 70){
+    console.log('Your got a' + 'C' + '(' + grade + '%' + ')!')
 }
