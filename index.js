e7b77478e2a926cd4eac3ffbe14e206a6c9f6490
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-12 11:58:48 +0530

    grade B greater than 80

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let grade = 67
 if(grade <= 100 && grade >= 90){
     console.log('Your got a ' + 'A' + '(' + grade + '%' + ')!')
+}else if(grade >= 80){
+ console.log('Your got a '+ 'B' + '(' + grade + '%' + ')!')
 }
