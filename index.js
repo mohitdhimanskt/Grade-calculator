54860434ea1c67ece202e8be7285bee92654cc82
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-12 13:03:40 +0530

    method for grade

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let grade = 67
 if(grade <= 100 && grade >= 90){
     console.log('Your got a ' + 'A' + '(' + grade + '%' + ')!')
-}else if(grade >= 80){
- console.log('Your got a '+ 'B' + '(' + grade + '%' + ')!')
-}else if(grade >= 70){
-    console.log('Your got a' + 'C' + '(' + grade + '%' + ')!')
-}else if(grade >= 60){
-    console.log('Your got a' + 'D' + '(' + grade + '%' + ')!') 
-}else if(grade >= 0){
-    console.log('Your got a' + 'E' + '(' + grade + '%' + ')!')
+}else if(grade >=80){
+    console.log('Your got a ' + 'B' + '(' + grade + '%' + ')!')
+}else if(grade >=70){
+    console.log('Your got a ' + 'C' + '(' + grade + '%' + ')!')
+}else if(grade >=60){
+    console.log('Your got a ' + 'D' + '(' + grade + '%' + ')!')
+}else if(grade >=0){
+    console.log('Your got a ' + 'E' + '(' + grade + '%' + ')!')
+}else{
+    console.log('You have enter the wrong value.')
 }
